cc461df9d3476c44433200478fa76dab3c30bc3b
Dongseon Lee <eastline@maycan.co.kr>
2021-05-24 14:00:11 +0900

    #Day1. Set Up

---
+++ README.md
@@ -0,0 +1,3 @@
+# NomadCoffee
+
+NomadCoffee Backend.
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+require("dotenv").config();
+import * as http from "http";
+import * as express from "express";
+import * as logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema";
+
+const PORT = process.env.PORT;
+const apollo = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+const app = express();
+app.use(logger("tiny"));
+apollo.applyMiddleware({ app });
+app.use("/static", express.static("uploads"));
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}/ ✅`);
+});
