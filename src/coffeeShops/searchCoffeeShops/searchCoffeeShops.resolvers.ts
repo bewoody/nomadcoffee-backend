895eefc878988cd08ac935bc98711b2676f7367c
Dongseon Lee <eastline@maycan.co.kr>
2021-06-17 16:47:41 +0900

    Modified: searchCoffeeShops logic

--- src/coffeeShops/searchCoffeeShops/searchCoffeeShops.resolvers.ts
+++ src/coffeeShops/searchCoffeeShops/searchCoffeeShops.resolvers.ts
@@ -4,7 +4,10 @@ import { SearchCoffeeShopsInput } from "./searchCoffeeShops.typeDefs";
 const resolvers: Resolvers = {
   Query: {
     searchCoffeeShops: (_, { keyword }: SearchCoffeeShopsInput, { client }) =>
-      client.coffeeShop.findMany({ where: { name: { startsWith: keyword } } }),
+      client.coffeeShop.findMany({
+        where: { name: { startsWith: keyword } },
+        include: { photos: { select: { url: true } } },
+      }),
   },
 };
 
